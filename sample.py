6a46b8db1ebc897b32bdce5b740d2cfcd7484f5f
nemon- <nemon@aliyun.com>
2019-04-28 11:36:07 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
 from simple_git_download import git_down
 git_down_agent = git_down()
-git_down_agent.showHelp()
 git_down_agent.main({'arr_src':'/nemon-/simple_git_download.git','target_path':['.'],'output_type':'wget','s_src_root':'https://github.com'})
